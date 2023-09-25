c8fb36d3efcfc381006efcf17abd3e43617d8b4e
Ahmad Sulthon Al Ayyubi <dafiqalimi17@gmail.com>
2023-09-25 13:33:04 +0700

    Initial commit

---
+++ API/Models/Employees.cs
@@ -0,0 +1,16 @@
+namespace API.Models;
+
+public class Employees
+{
+    public Guid Guid { get; set; }
+    public string Nik { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public int Gender { get; set; }
+    public DateTime HiringDate { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+}
---
+++ API/Models/Roles.cs
@@ -0,0 +1,10 @@
+namespace API.Models;
+
+public class Roles
+{
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+
+}
